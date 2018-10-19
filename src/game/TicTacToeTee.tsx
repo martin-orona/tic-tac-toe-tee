27903c9a72da829ce93b80fe1f932a669ba93d56
MartinGT60\Martin Orona <martin.g.orona@gmail.com>
2018-10-18 21:12:38 -0700

    Moved PlayerEditor into directory "player", which will house player related components.

--- src/game/TicTacToeTee.tsx
+++ src/game/TicTacToeTee.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
-import PlayerEditor from "./components/PlayerEditor";
+import PlayerEditor from "./components/player/PlayerEditor";
 import { IPlayer, WhichPlayer } from "./sharedInterfaces";
 
 import "./TicTacToeTee.css";
--- src/game/components/player/PlayerEditor.tsx
+++ src/game/components/player/PlayerEditor.tsx
@@ -1,22 +1,22 @@
 import * as React from "react";
-import { IPlayer } from "../sharedInterfaces";
+import { IPlayer } from "../../sharedInterfaces";
 
 import "./PlayerEditor.css";
 
-import Avatar01 from "../images/avatar01.png";
-import Avatar02 from "../images/avatar02.png";
-import Avatar03 from "../images/avatar03.png";
-import Avatar04 from "../images/avatar04.png";
-import Avatar05 from "../images/avatar05.png";
-import Avatar06 from "../images/avatar06.png";
-import Avatar07 from "../images/avatar07.png";
-import Avatar08 from "../images/avatar08.png";
-import Avatar09 from "../images/avatar09.png";
-import Avatar10 from "../images/avatar10.png";
-import Avatar11 from "../images/avatar11.png";
-import Avatar12 from "../images/avatar12.png";
-import Avatar13 from "../images/avatar13.png";
-import Avatar14 from "../images/avatar14.png";
+import Avatar01 from "../../images/avatar01.png";
+import Avatar02 from "../../images/avatar02.png";
+import Avatar03 from "../../images/avatar03.png";
+import Avatar04 from "../../images/avatar04.png";
+import Avatar05 from "../../images/avatar05.png";
+import Avatar06 from "../../images/avatar06.png";
+import Avatar07 from "../../images/avatar07.png";
+import Avatar08 from "../../images/avatar08.png";
+import Avatar09 from "../../images/avatar09.png";
+import Avatar10 from "../../images/avatar10.png";
+import Avatar11 from "../../images/avatar11.png";
+import Avatar12 from "../../images/avatar12.png";
+import Avatar13 from "../../images/avatar13.png";
+import Avatar14 from "../../images/avatar14.png";
 
 const avatars: string[] = [
   Avatar01,
