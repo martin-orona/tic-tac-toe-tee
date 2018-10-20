58eb4abe9b2f40ebeb71f4d8c9f5885e8576fb6e
MartinGT60\Martin Orona <martin.g.orona@gmail.com>
2018-10-19 21:56:48 -0700

    Stopped displaying the default App UI.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./App.css";
 import TicTacToeTee from "./game/TicTacToeTee";
 
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 
 import Avatar2 from "./game/images/avatar01.png";
 import Avatar1 from "./game/images/avatar02.png";
@@ -15,12 +15,12 @@ class App extends React.Component {
           player1={{ name: "Jen", avatarUrl: Avatar1 }}
           player2={{ name: "Ben", avatarUrl: Avatar2 }}
         />
-        <br />
+        {/* <br />
         <br />
         <hr />
         <br />
-        <br />
-        <div className="App">
+        <br /> */}
+        {/* <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
@@ -28,7 +28,7 @@ class App extends React.Component {
           <p className="App-intro">
             To get started, edit <code>src/App.tsx</code> and save to reload.
           </p>
-        </div>
+        </div> */}
       </div>
     );
   }
